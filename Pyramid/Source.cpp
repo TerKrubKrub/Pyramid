f286c4715b48673d467f24740d47ba6c33d1f0b3
Jirakan Koollasing <TerKrub084@gmail.com>
2020-08-21 19:56:26 +0700

    I+

--- Pyramid/Source.cpp
+++ Pyramid/Source.cpp
@@ -3,14 +3,18 @@ int main()
 {
 	int i, j, x;
 	scanf_s("%d", &x);
-	for (i = 1; i <= x; i++)
+	if (x >= 0)
 	{
-		for (j = 1; j <= (x * 2) - 1; j++)
+		for (i = 1; i <= x; i++)
 		{
-			if (j + i > x && j - i <= x - 1) printf("*");
-			else printf(" ");
+			for (j = 1; j <= (x * 2) - 1; j++)
+			{
+				if (j + i > x && j - i <= x - 1) printf("*");
+				else printf(" ");
+			}
+			printf("\n");
 		}
-		printf("\n");
 	}
+	else printf("Error");
 	return 0;
 }
